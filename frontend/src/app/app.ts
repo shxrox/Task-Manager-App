b9f6c8210d563a8a84a812a96380d49110dca0fe
shxrox <sharondeva8@gmail.com>
2026-03-19 20:15:49 +0530

    remove boilerplate template and set up root layout

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -1,12 +1,27 @@
-import { Component, signal } from '@angular/core';
+// import { Component, signal } from '@angular/core';
+// import { RouterOutlet } from '@angular/router';
+
+// @Component({
+//   selector: 'app-root',
+//   imports: [RouterOutlet],
+//   templateUrl: './app.html',
+//   styleUrl: './app.css'
+// })
+// export class App {
+//   protected readonly title = signal('frontend');
+// }
+import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavbarComponent } from './shared/components/navbar/navbar.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  // We MUST include NavbarComponent and RouterOutlet in this imports array
+  imports: [RouterOutlet, NavbarComponent],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
-export class App {
-  protected readonly title = signal('frontend');
+export class AppComponent {
+  title = 'Task Manager';
 }
