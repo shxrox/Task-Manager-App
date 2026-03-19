2e68cd1c7c8c0c514e8ea87ff360126d325a313a
shxrox <sharondeva8@gmail.com>
2026-03-19 19:52:02 +0530

    register jwtInterceptor in app configuration

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -1,7 +1,8 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
-import { provideHttpClient } from '@angular/common/http'; 
-import { provideAnimationsAsync } from '@angular/platform-browser/animations/async'; 
+import { provideHttpClient, withInterceptors } from '@angular/common/http'; // 
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { jwtInterceptor } from './core/interceptors/jwt.interceptor'; // 
 
 import { routes } from './app.routes';
 
@@ -9,7 +10,8 @@ export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
-    provideHttpClient(),
+  
+    provideHttpClient(withInterceptors([jwtInterceptor])), 
     provideAnimationsAsync()
   ]
 };
