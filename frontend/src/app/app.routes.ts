c9cebc6ac6c7c2f6e7d92b2282d4b96991165ece
shxrox <sharondeva8@gmail.com>
2026-03-19 20:31:06 +0530

    implement routing and fix bootstrap configuration

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,15 +1,30 @@
 import { Routes } from '@angular/router';
+import { authGuard } from './core/guards/auth.guard'; 
 
-
-{ 
-  path: 'tasks/new', 
-  loadComponent: () => import('./features/tasks/task-form/task-form.component').then(m => m.TaskFormComponent),
-  canActivate: [authGuard] 
-},
-{ 
-  path: 'tasks/edit/:id', 
-  loadComponent: () => import('./features/tasks/task-form/task-form.component').then(m => m.TaskFormComponent),
-  canActivate: [authGuard] 
-},
-
-export const routes: Routes = [];
+export const routes: Routes = [
+  { 
+    path: 'login', 
+    loadComponent: () => import('./features/auth/login/login.component').then(m => m.LoginComponent) 
+  },
+  { 
+    path: 'register', 
+    loadComponent: () => import('./features/auth/register/register.component').then(m => m.RegisterComponent) 
+  },
+  { 
+    path: 'tasks', 
+    loadComponent: () => import('./features/tasks/task-list/task-list.component').then(m => m.TaskListComponent),
+    canActivate: [authGuard] 
+  },
+  { 
+    path: 'tasks/new', 
+    loadComponent: () => import('./features/tasks/task-form/task-form.component').then(m => m.TaskFormComponent),
+    canActivate: [authGuard] 
+  },
+  { 
+    path: 'tasks/edit/:id', 
+    loadComponent: () => import('./features/tasks/task-form/task-form.component').then(m => m.TaskFormComponent),
+    canActivate: [authGuard] 
+  },
+  { path: '', redirectTo: '/tasks', pathMatch: 'full' },
+  { path: '**', redirectTo: '/login' }
+];
